098c71fc9e831277abf4cba7d27849a3a0816d31
heoseongjin <lucasheo.develop@gmail.com>
2023-12-24 23:29:54 +0900

    chore: project initial commit

---
+++ Notion-API-App/Notion-API-App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Notion-API-App
+//
+//  Created by 허성진 on 2023/12/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
